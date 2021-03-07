e4443a4b651b2a5477e7f462d73f1c515dabd447
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-03-07 20:41:57 +0100

    php mailer backend from vue form

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,20 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Dotenv\Validator;
 use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller{
 
 
 
     public function sendMail(Request $data){
-        $data->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255',
-            'subject' => 'required|string|max:100',
-            'message' => 'required|string|max:1000',
-        ]);
+//        $data->validate([
+//            'name' => 'required|string|max:255',
+//            'email' => 'required|string|email|max:255',
+//            'subject' => 'required|string|max:100',
+//            'message' => 'required|string|max:1000',
+//        ]);
+        $data = array('name'=>$data->name,
+            'email'=>$data->email,
+            'subject'=>$data->subject,
+            'message'=>$data->message
+        );
+        Mail::send('mail',$data,function($message){
+            $message->to('filipp-tts@outlook.com','to web developer');
+            $message->subject('test subject');
+            $message->cc('filipp-tts@outlook.com');
+        });
 
     }
 
