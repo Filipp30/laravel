1177cb91d6062c66b7bc85e02d967e9b05024ea2
Filipp <67765182+Filipp30@users.noreply.github.com>
2021-03-28 23:00:04 +0200

    AdmitChatController witch waiting list->> get all items method

---
+++ app/Http/Controllers/AdminChatController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChatWaitingList;
+use Illuminate\Http\Request;
+
+class AdminChatController extends Controller
+{
+    public function chat_waiting_list(){
+       return ChatWaitingList::all();
+    }
+}
